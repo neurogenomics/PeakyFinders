fc09d856e2cfe8dd63a26d9abc54dd82bf960151
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-07-13 13:41:10 -0400

    decompress local bedgraphs

--- R/download_bedgraph.R
+++ R/download_bedgraph.R
@@ -1,15 +1,19 @@
-download_bedgraph <- function(bedgraph_path){
+download_bedgraph <- function(bedgraph_path,
+                              decompress=TRUE){
     if(!is_local(bedgraph_path)){
         bedgraph_path2 <- paste(tempdir(),
                                 basename(bedgraph_path),
                                 sep = "/")
         utils::download.file(url = bedgraph_path, 
-                             destfile = bedgraph_path2)
-        if(R.utils::isGzipped(bedgraph_path2)){
-            bedgraph_path2 <- R.utils::gunzip(bedgraph_path2, 
-                                              overwrite=TRUE)
-        } 
+                             destfile = bedgraph_path2) 
         bedgraph_path <- bedgraph_path2
     }
+    #### Must be unzipped to be recognized by MACSr ####
+    if(isTRUE(decompress)){
+        if(R.utils::isGzipped(bedgraph_path)){
+            bedgraph_path <- R.utils::gunzip(bedgraph_path, 
+                                             overwrite=TRUE)
+        } 
+    } 
     return(bedgraph_path)
 }
